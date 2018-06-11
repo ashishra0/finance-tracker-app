eb136a2fc7ad2e588aa9df198b7cbcb8400b2e02
Ashish Rao <ashishrao2598@gmail.com>
2018-06-11 16:19:57 +0530

    Add Ajax

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,15 +1,17 @@
 class StocksController < ApplicationController
   def search
-    if params[:stock].present? # if there is any string present
-      @stock =  Stock.new_from_lookup(params[:stock]) # pass the string in this method
+    if params[:stock].present?
+      @stock = Stock.new_from_lookup(params[:stock])
       if @stock
-        render 'users/my_portfolio'
+        respond_to do |format|
+          format.js { render partial: 'users/result' }
+        end
       else
-        flash[:danger] = "Invalid ticker, Enter a valid ticker"
+        flash[:danger] = "You have entered an incorrect symbol"
         redirect_to my_portfolio_path
       end
     else
-      flash[:danger] = "Please enter a ticker"
+      flash[:danger] = "You have entered an empty search string"
       redirect_to my_portfolio_path
     end
   end
